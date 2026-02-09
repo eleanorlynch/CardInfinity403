30b9543624e80c9973d3b1574cf7d1c62929d032
eleanorlynch <114894686+eleanorlynch@users.noreply.github.com>
2026-02-09 15:15:35 -0800

    Update GameMoveTests.mjs
    Attempting to see if I can access GameMove from this file

--- test/GameMoveTests.mjs
+++ test/GameMoveTests.mjs
@@ -1,5 +1,9 @@
 import assert from "node:assert";
 
+// Attempting to see if I can access GameMove
+
+var testMove = new GameMove();
+
 describe("Array", function () {
   describe("#indexOf()", function () {
     it("should return -1 when the value is not present", function () {
