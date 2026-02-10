4f046073906127700fc78e4eacc6d8471cd1777c
eleanorlynch <114894686+eleanorlynch@users.noreply.github.com>
2026-02-09 23:01:33 -0800

    Update GameMoveTests.mjs
    Added a Status so it can use GameStatus

--- test/GameMoveTests.mjs
+++ test/GameMoveTests.mjs
@@ -2,6 +2,7 @@ import assert from "node:assert";
 import { GameMove } from "../server/GameMove.js";
 import { GameStatus } from "../server/GameStatus.js";
 
-const Mover = new GameMove();
-Mover.createGame(123, [], []);
-assert.strictEqual(Mover.getGame, 123);
+const Status = new GameStatus(123, [], []);
+const Move = new GameMove();
+Move.createGame(123, [], []);
+assert.strictEqual(Move.getGame, 123);
