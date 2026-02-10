29e0060d83aa1310f5c7800833f23e399ec6ff11
eleanorlynch <114894686+eleanorlynch@users.noreply.github.com>
2026-02-09 23:07:12 -0800

    Update GameMoveTests.mjs
    Added Mocha wrapping for readability

--- test/GameMoveTests.mjs
+++ test/GameMoveTests.mjs
@@ -1,8 +1,13 @@
 import assert from "node:assert";
 import { GameMove } from "../server/GameMove.js";
 import { GameStatus } from "../server/GameStatus.js";
-
-const Status = new GameStatus(123, [], []);
-const Move = new GameMove();
-const game = Move.createGame(123, [], []);
-assert.strictEqual(Move.getGame(123), game);
+describe("GameMove", function () {
+  describe("#getGame()", function () {
+    it("should return the correct game when given its gameid", function () {
+      const Status = new GameStatus(123, [], []);
+      const Move = new GameMove();
+      const game = Move.createGame(123, [], []);
+      assert.strictEqual(Move.getGame(123), game);
+    });
+  });
+});
