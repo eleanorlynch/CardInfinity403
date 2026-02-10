79b89572478a8e6d29b9b691f835f40989375367
eleanorlynch <114894686+eleanorlynch@users.noreply.github.com>
2026-02-09 22:52:52 -0800

    Update GameMoveTests.mjs
    Adding the import for GameStatus back since the cause was identified as being there

--- test/GameMoveTests.mjs
+++ test/GameMoveTests.mjs
@@ -1,7 +1,7 @@
 import assert from "node:assert";
 import { GameMove } from "../server/GameMove.js";
-//import { GameStatus } from "../server/GameStatus.js";
+import { GameStatus } from "../server/GameStatus.js";
 
 const Mover = new GameMove();
 Mover.createGame(123, [], []);
-assert.equals(Mover.getGame, 123);
+assert.strictEqual(Mover.getGame, 123);
