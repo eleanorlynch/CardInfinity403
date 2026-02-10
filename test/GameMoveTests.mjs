e3b64592ecba2501edc24fa91b2d78752bc4f07c
eleanorlynch <114894686+eleanorlynch@users.noreply.github.com>
2026-02-09 23:21:14 -0800

    Update GameMoveTests.mjs
    Made assert check createGame by specific values

--- test/GameMoveTests.mjs
+++ test/GameMoveTests.mjs
@@ -7,15 +7,18 @@ describe("GameMove", function () {
     it("should return the correct game when told to create one based on certain properties", function () {
       const Move = new GameMove();
       const game = new GameStatus(123, [], []);
-      assert.equal(Move.createGame(123, [], []), game);
+      const game2 = Move.createGame(123, [], []);
+      assert.equal(game2.getGameId(), game.getGameId());
+      assert.equal(game2.getRuleset(), game.getRuleset());
+      assert.equal(game2.getPlayers(), game.getPlayers());
     });
   });
   describe("#getGame()", function () {
     it("should return the correct game when given its gameid", function () {
       //const Status = new GameStatus(123, [], []);
       const Move = new GameMove();
-      const game2 = Move.createGame(123, [], []);
-      assert.strictEqual(Move.getGame(123), game2);
+      const game3 = Move.createGame(123, [], []);
+      assert.strictEqual(Move.getGame(123), game3);
     });
   });
 });
