31f61a9fad58de8452404fc9db58299103aab98b
eleanorlynch <114894686+eleanorlynch@users.noreply.github.com>
2026-02-09 22:45:16 -0800

    Update GameMoveTests.mjs
    Importing GameStatus so it can be used

--- test/GameMoveTests.mjs
+++ test/GameMoveTests.mjs
@@ -1,5 +1,6 @@
 import assert from "node:assert";
 import { GameMove } from "../server/GameMove.js";
+import { GameStatus } from "../server/GameStatus.js";
 
 const Mover = new GameMove();
 Mover.createGame(123, [], []);
