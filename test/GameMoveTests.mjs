c7592008b82e4ea3076fa2068763c97fbb12265a
eleanorlynch <114894686+eleanorlynch@users.noreply.github.com>
2026-02-10 11:26:19 -0800

    Update GameMoveTests.mjs
    Added a test for getGameId

--- test/GameMoveTests.mjs
+++ test/GameMoveTests.mjs
@@ -3,15 +3,19 @@ import { GameMove } from "../server/GameMove.js";
 import { GameStatus } from "../server/GameStatus.js";
 
 describe("GameMove", function () {
+  describe('#getGameId()", function () {
+    it("should return the correct gameid for a game", function () {
+      const players = [1];
+      const ruleset = [2];
+      game4 = Move.createGame(123, players, ruleset);
+      assert.equal(game4.getGameId(), 123);
   describe("#createGame()", function () {
     it("should return the correct game when told to create one based on certain properties", function () {
       const Move = new GameMove();
-      const players = [1];
-      const ruleset = [2];
       const game = new GameStatus(123, ruleset, players);
       const game2 = Move.createGame(123, ruleset, players);
       assert.equal(game2.getGameId(), game.getGameId());
-      assert.equal(game2.getRuleset(), 2);
+      assert.equal(game2.getRuleset(), 3);
       assert.equal(game2.getPlayers(), game.getPlayers());
     });
   });
