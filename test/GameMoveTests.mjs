149fdd520b57cdf2d882b5f2134f5e72bd5375fd
eleanorlynch <114894686+eleanorlynch@users.noreply.github.com>
2026-02-09 23:11:34 -0800

    Update GameMoveTests.mjs
    Adding a new test and commenting out a potentially unnecessary line

--- test/GameMoveTests.mjs
+++ test/GameMoveTests.mjs
@@ -1,11 +1,20 @@
 import assert from "node:assert";
 import { GameMove } from "../server/GameMove.js";
 import { GameStatus } from "../server/GameStatus.js";
+
 describe("GameMove", function () {
+  describe("#createGame()", function () {
+    it("should return the correct game when told to create one based on certain properties", function () {
+      const Move = new GameMove();
+      const game = new GameStatus(gameId, ruleset, players);
+      assert.strictEqual(Move.createGame(123, [], []));
+    }
+  }
   describe("#getGame()", function () {
     it("should return the correct game when given its gameid", function () {
-      const Status = new GameStatus(123, [], []);
+      //const Status = new GameStatus(123, [], []);
       const Move = new GameMove();
+      const game = new GameStatus(gameId, ruleset, players);
       const game = Move.createGame(123, [], []);
       assert.strictEqual(Move.getGame(123), game);
     });
