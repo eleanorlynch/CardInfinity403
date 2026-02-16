22bdcfcb05351cbd0a584cece5314cdd60464ad8
asdf6921 <gaojiayi.jerry@gmail.com>
2026-02-15 20:01:16 -0800

    Updated gameboard to be able to play single player game of draw card and play card. Duplicated GameMove, GameStatus, GameWinner to phaser in order to be able to be accessed by Game.ts

--- phaser-multiplayer-template/packages/client/src/utils/server/GameWinner.js
+++ phaser-multiplayer-template/packages/client/src/utils/server/GameWinner.js
@@ -1,12 +1,21 @@
-class GameWinner {
+export class GameWinner {
     // Check if any player has won
     static checkWinner(gameState) {
         // Simple win condition: player with empty hand wins
-        if (gameState.playerHand.length === 0) {
+        if (gameState.myHand && gameState.myHand.length === 0) {
             return {
                 winner: gameState.currentTurn,
                 winCondition: 'empty_hand',
                 message: "Player has no cards left!"
+            };
+        }
+        
+        // Check if game is already over
+        if (gameState.gameOver && gameState.winner) {
+            return {
+                winner: gameState.winner,
+                winCondition: 'game_over',
+                message: "Game is over!"
             };
         }
         
