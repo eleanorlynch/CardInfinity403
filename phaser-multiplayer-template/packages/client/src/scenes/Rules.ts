02b944f62bf94ff599706d92a4b450f08e91589b
heather <hconeybe@uw.edu>
2026-02-23 22:16:37 -0800

    navigation buttons working

--- phaser-multiplayer-template/packages/client/src/scenes/Rules.ts
+++ phaser-multiplayer-template/packages/client/src/scenes/Rules.ts
@@ -11,6 +11,7 @@ export class Rules extends Scene {
 
   rulesets_temp_delete_later: Ruleset[] = [];
   rulesets: Map<string, Phaser.GameObjects.Container> = new Map();
+  page_number: number = 0;
 
   create() {
 
@@ -55,6 +56,7 @@ export class Rules extends Scene {
     });
 
     backButton.on("pointerdown", () => {
+      this.rulesets_temp_delete_later = [];
       this.scene.start("MainMenu");
     });
 
@@ -102,10 +104,71 @@ export class Rules extends Scene {
       .strokeRoundedRect(width * 0.4, height * 0.82, width * 0.2, height * 0.1);
 
 
+    const navigation_right_button = this.add.text(width * 0.55, height * 0.83, '>', {
+      fontFamily: 'Arial',
+      fontSize: '3.5rem',
+      color: '#101814',
+      align: 'center',
+      fixedWidth: 50,
+      backgroundColor: '#ffffff'
+    })
 
+    navigation_right_button.setInteractive({ useHandCursor: true });
+
+    navigation_right_button.on('pointerover', () => {
+      navigation_right_button.setBackgroundColor('#EBC9B3');
+    });
+
+    navigation_right_button.on('pointerout', () => {
+      navigation_right_button.setBackgroundColor('#ffffff');
+    });
+
+    navigation_right_button.on('pointerdown', () => {
+      this.handle_navigation_click(1);
+      // TODO: disable buttons when upper/lower limit reached (# rules / 5 (int div) === 0 || pagenum === 0)
+      // depends on if this is left or right
+      page_number_indicator.setText('' + this.page_number);
+    })
+
+    const navigation_left_button = this.add.text(width * 0.41, height * 0.83, '<', {
+      fontFamily: 'Arial',
+      fontSize: '3.5rem',
+      color: '#101814',
+      align: 'center',
+      fixedWidth: 50,
+      backgroundColor: '#ffffff'
+    })
+
+    navigation_left_button.setInteractive({ useHandCursor: true });
+
+
+    navigation_left_button.on('pointerover', () => {
+      navigation_left_button.setBackgroundColor('#EBC9B3');
+    });
+
+    navigation_left_button.on('pointerout', () => {
+      navigation_left_button.setBackgroundColor('#ffffff');
+    });
+
+    navigation_left_button.on('pointerdown', () => {
+      this.handle_navigation_click(-1);
+      page_number_indicator.setText('' + this.page_number);
+      // TODO: disable buttons when upper/lower limit reached (# rules / 5 (int div) === 0 || pagenum === 0)
+      // depends on if this is left or right
+    })
+
+    const page_number_indicator = this.add.text(width * 0.48, height * 0.84, String(this.page_number), {
+      fontFamily: 'Arial',
+      fontSize: '2.5rem',
+      color: '#101814',
+      align: 'center',
+      fixedWidth: 50,
+    })
 
     rules_container.add(rules_container_bg).add(navigation_container);
-    navigation_container.add(navigation_container_bg);
+    navigation_container.add(navigation_container_bg)
+      .add(navigation_left_button)
+      .add(navigation_right_button);
 
 
     //populate rules
@@ -116,12 +179,20 @@ export class Rules extends Scene {
     this.rulesets_temp_delete_later.push(new Ruleset("e"));
     this.rulesets_temp_delete_later.push(new Ruleset("th is me"));
 
-
     this.populate_rulesets(width, height, container_width);
+
   }
 
 
   //Helper fns
+
+  handle_navigation_click(increment: number) {
+    if (increment > 0 || this.page_number > 0){
+        this.page_number += increment;
+    }
+    // not really sure if this should be a function?
+    // TODO: check style guide
+  }
 
   populate_rulesets(width: number, height: number, container_width: number) {
     const startY = height * 0.27;
